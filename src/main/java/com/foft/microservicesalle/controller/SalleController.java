25c85d730c747232525627b59691339d5b60d24a
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-06-01 12:34:21 +0100

    Third Part

--- src/main/java/com/foft/microservicesalle/controller/SalleController.java
+++ src/main/java/com/foft/microservicesalle/controller/SalleController.java
@@ -5,13 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.foft.microservicesalle.modele.Salle;
 import com.foft.microservicesalle.repository.SalleRepository;
@@ -19,6 +13,7 @@ import com.foft.microservicesalle.service.SalleService;
 
 
 @RestController
+@RequestMapping("MicroSalle")
 public class SalleController {
     
 
